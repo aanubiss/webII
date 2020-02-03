b1daf5f31b9e03dbb919a01d9837c7ad794c497c
aanubiss <narek.kazaryan.2014@inbox.ru>
2020-02-03 14:37:44 +0400

    Ok

---
+++ script.js
@@ -0,0 +1,10 @@
+function dSideText(){
+
+	document.getElementById('sideText').innerHTML = 'ВЫБЕРИ ТЕМНУЮ СТОРОНУ!';
+}
+function lSideText(){
+	document.getElementById('sideText').innerHTML = 'ВЫБЕРИ СВЕТЛУЮ СТОРОНУ!';
+}
+function delText(){
+	document.getElementById('sideText').innerHTML = ' '
+}
